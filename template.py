1f136088dbf776b3cce99eed3f1f8347d08b1de1
Erik Dyrelius <erik.dyrelius@gmail.com>
2025-11-12 15:52:42 +0100

    Add template python file

---
+++ template.py
@@ -0,0 +1,11 @@
+inp = """input your actual mentor pattern string here"""
+
+def problem():
+    return 0
+
+def main():
+    print(f"All mentors (examples): {problem()}")
+    print(f"All mentors (actual): {problem()}")
+    
+if __name__ == "__main__":
+    main()
